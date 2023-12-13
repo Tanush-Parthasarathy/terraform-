97888276ce35462033f60df1f6c0abffe9c6228f
jackkgit <153727514+jackkgit@users.noreply.github.com>
2023-12-13 17:54:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,3 @@
-# Azure provider configuration
-variable "client_id" {
-  description = "Azure client ID."
-}
-
-variable "client_secret" {
-  description = "Azure client secret."
-}
-
-variable "subscription_id" {
-  description = "Azure subscription ID."
-}
-
-variable "tenant_id" {
-  description = "Azure tenant ID."
-}
-
-
 
 
 
