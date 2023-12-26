0037584781f4950752091b4cf7cdd7d628165b98
jackkgit <153727514+jackkgit@users.noreply.github.com>
2023-12-26 13:10:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_virtual_network" "example" {
   name                = "jack-virtual-network"
   resource_group_name = data.azurerm_resource_group.existing.name
   location            = data.azurerm_resource_group.existing.location
-  address_space       = "[10.0.0.0/16]"
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "public_subnet" {
