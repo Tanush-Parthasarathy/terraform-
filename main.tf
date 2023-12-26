ca5a42129894b41623e0b6de64d4e1247ada2c01
jackkgit <153727514+jackkgit@users.noreply.github.com>
2023-12-26 14:07:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.0.0"  # Replace with the desired version
+      version = ">= 2.0, < 3.0"
     }
   }
 }
