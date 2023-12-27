0e01c6d53ee3682cbac64d1ac7fd162dcde175f4
jackkgit <153727514+jackkgit@users.noreply.github.com>
2023-12-27 11:34:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,14 +38,14 @@ resource "azurerm_subnet" "public_subnet" {
   name                 = "public-subnet"
   resource_group_name  = data.azurerm_resource_group.existing.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes       = "10.0.1.0/24"
+  address_prefixes       = ["10.0.1.0/24"]
 }
 
 resource "azurerm_subnet" "private_subnet" {
   name                 = "private-subnet"
   resource_group_name  = data.azurerm_resource_group.existing.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes       = "10.0.4.0/24"
+  address_prefixes       = ["10.0.4.0/24"]
 }
 
 
